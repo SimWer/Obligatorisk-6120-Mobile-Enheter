abd82e9fd9c172b5234ce7ffe28ea6c9f37f2dfc
SimWer <simon.wernersson@live.no>
2019-09-05 19:55:28 +0200

    Finished login screen

--- app/src/main/java/com/oblig/boskartoteket/MainActivity.java
+++ app/src/main/java/com/oblig/boskartoteket/MainActivity.java
@@ -3,12 +3,22 @@ package com.oblig.boskartoteket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
+    private EditText user_input, password_input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.activity_main);
+        user_input = findViewById(R.id.email_input);
+        password_input = findViewById(R.id.password_input);
+
     }
+
 }

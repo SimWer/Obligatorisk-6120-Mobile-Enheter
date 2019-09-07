5a83f73cf39b5f9c979a9fcc1ad4e7041bd31669
Nataniel <nataniel.haraldseth@hotmail.com>
2019-09-07 15:13:03 +0200

    Bestillings fragment lagt til, blant andre endringer.

---
+++ app/src/main/java/com/oblig/boskartoteket/User_Settings.java
@@ -0,0 +1,31 @@
+package com.oblig.boskartoteket;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class User_Settings extends Fragment {
+
+
+    public User_Settings() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user__settings, container, false);
+    }
+
+}

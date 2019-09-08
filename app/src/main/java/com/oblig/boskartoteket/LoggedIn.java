657ac39929e8b668cab9b541f0d9681c2b578441
Nataniel <nataniel.haraldseth@hotmail.com>
2019-09-08 15:39:59 +0200

    En masse endringer

--- app/src/main/java/com/oblig/boskartoteket/LoggedIn.java
+++ app/src/main/java/com/oblig/boskartoteket/LoggedIn.java
@@ -1,14 +1,54 @@
 package com.oblig.boskartoteket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class LoggedIn extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class LoggedIn extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+
+        //Loading the default fragment
+        loadFragment(new HjemFragment());
+
+        //Getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.nav_view);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch(menuItem.getItemId()) {
+            case R.id.menu_dates:
+                fragment = new HjemFragment();
+                break;
+            case R.id.menu_shopping_cart:
+                fragment = new BestillingFragment();
+                break;
+            case R.id.menu_settings:
+                fragment = new BrukerInnstillingFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //Switching Fragment
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+        return false;
     }
 }
